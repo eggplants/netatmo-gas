523d1f21583fc264ac9a1616fd48517002fe406e
eggplants <w10776e8w@yahoo.co.jp>
2025-05-01 01:34:46 +0000

    feat: scoring

---
+++ src/evaluate-environment-parameter.ts
@@ -0,0 +1,60 @@
+export function evaluateEnvironmentParameter(props: {
+  temperature: number
+  humidity: number
+  co2ppm: number
+  noiseDb: number
+  pressureHpa: number
+}) {
+  const {co2ppm, humidity, noiseDb, pressureHpa, temperature} = props
+  return {
+    co2ppm: [co2ppm, toStarRating(evaluateCo2(co2ppm))],
+    humidity: [humidity, toStarRating(evaluateHumidity(humidity))],
+    noiseDb: [noiseDb, toStarRating(evaluateNoise(noiseDb))],
+    pressureHpa: [pressureHpa, toStarRating(evaluatePressure(pressureHpa))],
+    temperature: [temperature, toStarRating(evaluateTemperature(temperature))],
+  }
+}
+
+function toStarRating(score: number): string {
+  return '★'.repeat(score) + '☆'.repeat(5 - score)
+}
+
+function evaluateTemperature(temp: number): number {
+  if (temp >= 22 && temp <= 25) return 5
+  if ((temp >= 20 && temp < 22) || (temp > 25 && temp <= 27)) return 4
+  if ((temp >= 18 && temp < 20) || (temp > 27 && temp <= 29)) return 3
+  if ((temp >= 16 && temp < 18) || (temp > 29 && temp <= 31)) return 2
+  return 1 // Temp <= 15 or temp >= 32
+}
+
+function evaluateHumidity(humidity: number): number {
+  if (humidity >= 45 && humidity <= 60) return 5
+  if ((humidity >= 40 && humidity < 45) || (humidity > 60 && humidity <= 65)) return 4
+  if ((humidity >= 35 && humidity < 40) || (humidity > 65 && humidity <= 70)) return 3
+  if ((humidity >= 30 && humidity < 35) || (humidity > 70 && humidity <= 75)) return 2
+  return 1 // Humidity < 30 or > 75
+}
+
+function evaluateCo2(co2ppm: number): number {
+  if (co2ppm <= 600) return 5
+  if (co2ppm <= 800) return 4
+  if (co2ppm <= 1000) return 3
+  if (co2ppm <= 1500) return 2
+  return 1 // Co2ppm > 1500
+}
+
+function evaluateNoise(noiseDb: number): number {
+  if (noiseDb <= 30) return 5
+  if (noiseDb <= 40) return 4
+  if (noiseDb <= 55) return 3
+  if (noiseDb <= 70) return 2
+  return 1 // NoiseDb > 70
+}
+
+function evaluatePressure(hpa: number): number {
+  if (hpa >= 1010 && hpa <= 1020) return 5
+  if ((hpa >= 1000 && hpa < 1010) || (hpa > 1020 && hpa <= 1030)) return 4
+  if ((hpa >= 990 && hpa < 1000) || (hpa > 1030 && hpa <= 1040)) return 3
+  if ((hpa >= 980 && hpa < 990) || (hpa > 1040 && hpa <= 1050)) return 2
+  return 1 // Hpa < 980 or > 1050
+}
--- src/slack-message.ts
+++ src/slack-message.ts
@@ -1,3 +1,4 @@
+import {evaluateEnvironmentParameter} from './evaluate-environment-parameter.js'
 import {type NetatmoStationData, type NetatmoStationDevice} from './type.js'
 
 export function createSlackMessage(stationData: NetatmoStationData, serverDate: Date) {
@@ -19,13 +20,20 @@ export function createSlackMessage(stationData: NetatmoStationData, serverDate: 
 }
 
 function createSlackMessageForDevice(device: NetatmoStationDevice) {
+  const {co2ppm, humidity, noiseDb, pressureHpa, temperature} = evaluateEnvironmentParameter({
+    co2ppm: device.dashboard_data.CO2,
+    humidity: device.dashboard_data.Humidity,
+    noiseDb: device.dashboard_data.Noise,
+    pressureHpa: device.dashboard_data.Pressure,
+    temperature: device.dashboard_data.Temperature,
+  })
   return [
     `*${device.station_name}*`,
-    `*Temp.*: ${device.dashboard_data.Temperature}°C`,
-    `*Humidity*: ${device.dashboard_data.Humidity}%`,
-    `*CO2*: ${device.dashboard_data.CO2}ppm`,
-    `*Noise*: ${device.dashboard_data.Noise}dB`,
-    `*Pressure*: ${device.dashboard_data.Pressure}hPa`,
+    `*Temp.*: ${temperature[0]}°C	${temperature[1]}`,
+    `*Humidity*: ${humidity[0]}}%	${humidity[1]}`,
+    `*CO2*: ${co2ppm[0]}ppm	${co2ppm[1]}`,
+    `*Noise*: ${noiseDb[0]}dB	${noiseDb[1]}`,
+    `*Pressure*: ${pressureHpa[0]}hPa	${pressureHpa[1]}`,
   ].map((text) => ({
     fields: [{text, type: 'mrkdwn'}],
     type: 'section',
